fb30de73bc0a3ca87b90b4e27ef909c8f70c50d7
Annu <annupoddar444@gmail.com>
2023-07-16 12:04:59 +0530

    filtered top rated restorent using useState

--- src/Body.js
+++ src/Body.js
@@ -1,16 +1,26 @@
 import RestaurantCart from "./RestaurantCart";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+    let [listOfRestorent, setListOfRestorent] = useState(resList)
     return (
         <div className="body">
             <div className="search_box">
-                <h2>Search</h2>
+                <button
+                    onClick={()=>{
+                        const filteredList = listOfRestorent.filter(
+                            (res) => res.data.avgRating > 4
+                        );
+                        setListOfRestorent(filteredList);
+                    }}
+                >
+                High rating Restorents</button>
             </div>
 
             <div className="cart_container">
-              {resList.map((restorent) => (
-                <RestaurantCart resData={restorent}/>
+              {listOfRestorent.map((restorent) => (
+                <RestaurantCart key={restorent.data.id}  resData={restorent}/>
             ))}
             </div>
         </div>
